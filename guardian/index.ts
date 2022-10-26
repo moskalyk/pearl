507bf4718ca9e763427a4303af3cbed5a7de4d61
Morgan Moskalyk <morgan.moskalyk@protonmail.ch>
2022-10-26 13:06:22 -0400

    Add files via upload

--- guardian/index.ts
+++ guardian/index.ts
@@ -1,64 +1,90 @@
-// import test from 'tape'
-const TH = require('../index')
-// import naive from '../naive'
-// import crypto from 'crypto'
+'use strict'
 
-// function hash (a, b='') {
-//   return crypto.createHash('sha256').update(a).update(b).digest()
-// }
+var empty = Buffer.from('')
 
-var zeros_1mb = Buffer.alloc(1024*1024).fill(0)
-var vectors = [
-  {
-    input: '9',
-  },
-  {
-    //on a short input, the result is just the sha256 hash
-    input: '1',
-  },{
-    //on a short input, the result is just the sha256 hash
-    input: '4',
-  },
-  // {
-  //   input: Buffer.alloc(1024*1024*2).fill(0),
-  //   hash: '861890b487038d840e9d71d43bbc0fd4571453fb9d9b1f370caa3582a29b0ec7'
-  // },
-  // {
-  //   input: Buffer.alloc(1024*1024*2).fill(0),
-  //   hash: '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08'
-  // },
-  // {
-  //   //trailing blocks are promoted
-  //   input: Buffer.alloc(1024*1024*3).fill(0),
-  //   hash:
-  //     '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08'
-    
-  // },
-  // {
-  //   //if input is less than the block size, partical block is hashad at the end.
-  //   input: Buffer.alloc(1024*1024*2.5).fill(0),
-  //   hash: '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08'
-  // }
-];
+async function hash (a, b='', secretKey, bls) {
+    // get number of connections from flu
+    // const connections = 2**8
+    // const connections = bots + scooters
+    // const secretKey = bls.SecretKey.fromKeygen();
+    const aSig = secretKey.sign(Buffer.from(a), 'hex');
+    const bSig = secretKey.sign(Buffer.from(aSig) , 'hex')
+    // console.log("Is valid: ", signature.verify(publicKey, message));
+  return bSig
+}
 
-(async () => {
+function update(block, tree, secretKey, bls) {
+  // var h = crypto.createHash('sha256')
+  for(var i = 0; i < block.length; i++)
+    // h.update(block[i])
+    block[i] = secretKey.sign(Buffer.from(block[i]), 'hex')
+  
+  // 
+  var i = 0, _h = secretKey.sign(Buffer.from(Math.random().toString(), 'hex'))
+  do {
+    if(tree[i]) {
+      _h = hash(tree[i], _h, secretKey, bls)
+      tree[i] = null
+      i++
+    }
+    else {
+      tree[i] = _h
+      break;
+    }
+  } while(true)
+}
 
-  let bls = (await import("../node_modules/@chainsafe/bls/lib/blst-native/index.js")).default
-  const secretKey = bls.SecretKey.fromKeygen()
-  const trees = []
-  for(var i = 0 ; i < vectors.length; i++) {
-    const tree = new TH(1024*1024, secretKey)
-    const update = await tree.update(vectors[i].input)
-    const digest = await tree.digest()
-    trees.push(digest)
-    if(digest != undefined) console.log(digest.toBytes().toString())
-    // console.log(update.tree[i])
-    // console.log(digest)
+class TreeHash  {
+  block_size;
+  len
+  queue
+  tree
+  secretKey
+  ended
+  constructor (block_size=1024*1024, secretKey) {
+    this.block_size = block_size
+    this.len = 0
+    this.queue = []
+    this.tree = []
+    this.secretKey = secretKey
   }
-  console.log('why arent these different? naive.')
-  console.log(trees[0].toHex() )
-  console.log(trees[1].toHex() )
-  console.log(trees[2].toHex() )
-  console.log(trees[0].toHex() == trees[1].toHex())
-  console.log(trees[1].toHex() == trees[2].toHex())
-})()
+  async update (data) {
+    let bls = (await import("./node_modules/@chainsafe/bls/lib/blst-native/index.js")).default
+
+    //XXX TODO, actually, we want to be able to add more data, so that streaming files are possible...
+    if(this.ended) throw new Error('cannot add more data already hashed tree')
+    while(data.length) {
+      if(this.len + data.length < this.block_size) {
+        this.queue.push(data)
+        this.len += data.length
+        data = empty
+      }
+      else {
+        this.queue.push(data.slice(0, this.block_size - this.len))
+        data = data.slice(this.block_size - this.len)
+        update(this.queue, this.tree, this.secretKey, bls)
+        this.queue = []; this.len = 0
+      }
+    }
+    return this
+  }
+  async digest () {
+      let bls = (await import("./node_modules/@chainsafe/bls/lib/blst-native/index.js")).default
+      this.ended = true
+    
+     if(this.queue.length) {
+        update(this.queue, this.tree, this.secretKey, bls)
+        this.queue = []
+      }
+     return this.tree.filter(Boolean).reduce((a, b) => {
+        //the tree array is the most recent branches of the tree
+        //at the end, there may be null items, representing full subtrees
+        //if there are single hashes at the end of the tree,
+        //they are promoted to the next level. but we are iterating over the array from the lower levels
+        //so b is the earlier hash, a is the new hash, promoted, so hash(b, a)
+        return hash(b, a, this.secretKey, bls)
+    })
+  }
+}
+
+module.exports = TreeHash
